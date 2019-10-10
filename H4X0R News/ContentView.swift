fd527ab1f0fb1aa39369616e6bcfa9235f96fa5d
Oluwatobi <oladipupo_tobi@yahoo.com>
2019-10-10 18:37:26 +0100

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by user on 10/10/2019.
+//  Copyright © 2019 Oladipupo Oluwatobi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
